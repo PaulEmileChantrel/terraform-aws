51c43f8db83fc3568fd0efadc083c1d2efdf1072
PaulEmileChantrel <paul-emile.chantrel.1@ulaval.ca>
2023-10-18 21:50:01 -0400

    deploy instance

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ca-central-1"
+  access_key = ""
+  secret_key = ""
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-01a2cb0405fa1877b"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Helloworld"
+  }
 }
