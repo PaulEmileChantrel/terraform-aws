3d521be327095c595fc1d3ef52da0cbebd919ab4
PaulEmileChantrel <paul-emile.chantrel.1@ulaval.ca>
2023-10-18 23:11:28 -0400

    creating vpc and subnet

--- main.tf
+++ main.tf
@@ -4,6 +4,23 @@ provider "aws" {
   secret_key = ""
 }
 
+resource "aws_vpc" "my_vpc"{
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "production_vpc"
+  }
+}
+
+
+resource "aws_subnet" "subnet_1" {
+  vpc_id = aws_vpc.my_vpc.id
+  cidr_block = "10.0.0.0/24"
+  tags = {
+    Name = "production_subnet_1"
+  }
+}
+
+
 resource "aws_instance" "app_server" {
   ami           = "ami-01a2cb0405fa1877b"
   instance_type = "t2.micro"
