180bab5bc50bcb823697877bd22fdc322cffc81a
PaulEmileChantrel <paul-emile.chantrel.1@ulaval.ca>
2023-10-19 21:17:27 -0400

    adding ig, rt and sg

--- main.tf
+++ main.tf
@@ -4,6 +4,8 @@ provider "aws" {
   secret_key = ""
 }
 
+
+# 1. Creat VPC
 resource "aws_vpc" "my_vpc"{
   cidr_block = "10.0.0.0/16"
   tags = {
@@ -12,13 +14,107 @@ resource "aws_vpc" "my_vpc"{
 }
 
 
+
+
+# 2. Create Internet Gateway
+
+resource "aws_internet_gateway" "gw"{
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+# 3. Create Custom Route Table
+resource "aws_route_table" "prod_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "prod_route_table"
+  }
+}
+
+# 4. Create Subnet
 resource "aws_subnet" "subnet_1" {
   vpc_id = aws_vpc.my_vpc.id
-  cidr_block = "10.0.0.0/24"
+  cidr_block = "10.0.1.0/24"
+
   tags = {
     Name = "production_subnet_1"
   }
 }
+
+# 5. Associate subnet with route table
+resource "aws_route_table_association" "a"{
+  subnet_id = aws_vpc.subnet_1.id
+  route_table_id = aws_vpc.prod_route_table.id
+}
+
+# 6. Security Group Policy to allow port 22,80,443
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web"
+  description = "Allow web traffic"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+# 7. Create Network
+
+resource "aws_network_interface" "test" {
+  subnet_id       = aws_subnet.public_a.id
+  private_ips     = ["10.0.0.50"]
+  security_groups = [aws_security_group.web.id]
+
+  attachment {
+    instance     = aws_instance.test.id
+    device_index = 1
+  }
+}
+
 
 
 resource "aws_instance" "app_server" {
